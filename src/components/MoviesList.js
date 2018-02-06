9332a42d4bc743a28523bc6608b1c590751937d8
Jonathan <jonathanbry@comcast.net>
2018-02-06 08:14:03 -0800

    Jonathan Bry first commit

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,10 +1,13 @@
-// Import React from react.
+import React from react; // Import React from react.
+function MoviesList(movies) { // Inside the javascript expression, map the movies array
+const MovieArr = this.state.movies.map((movies, i) =>  // Create a const arrow function called MoviesList. // The function inside map should take in two arguments, movies and i (for index).
+    <li key={number.toString({i})}>{movies}</li>
+); // The list item should have an attribute called key, that takes in i as a javascript expression.
 
-// Create a const arrow function called MoviesList.
-// Return an unordered list
-// Inside the unordered list, embed a javascript expression (curly brackets).
-// Inside the javascript expression, map the movies array
-// The function inside map should take in two arguments, movie and i (for index).
-// The function should return a list item, with a javascript expression movie (the same variable as map).
-// The list item should have an attribute called key, that takes in i as a javascript expression.
+return ( // Return an unordered list
+    <ul>{Movies}</ul>
+); // Inside the unordered list, embed a javascript expression (curly brackets).
+// The function should return a list item, with a javascript expression movies (the same variable as map).
+}
+export default class MoviesList{};
 // Outside the arrow function, export the function MoviesList as a default.
